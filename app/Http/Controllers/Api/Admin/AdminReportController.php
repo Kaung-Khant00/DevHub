34e18337c0022469be5e8a01dc19ee6e75be516c
Kaung Khant <pep281908@gmail.com>
2025-09-22 23:15:27 +0630

    fetch repots

--- app/Http/Controllers/Api/Admin/AdminReportController.php
+++ app/Http/Controllers/Api/Admin/AdminReportController.php
@@ -13,13 +13,13 @@ class AdminReportController extends Controller
         $page = request()->query('page', 1);
         $per_page = request()->query('per_page', 10);
         $type = request()->query('type');
-        $reports = Report::
-            when($type, function ($query) use ($type) {
+        logger($type);
+        $reports = Report::when($type, function ($query) use ($type) {
                 return $query->where('reportable_type', $type);
             })
-            ->with('reportable')
-            ->with('reporter')
+            ->with(['reportable','reporter'])
             ->paginate($per_page, ['*'], 'page', $page);
+            logger($reports);
         return response()->json([
             'reports' => $reports,
         ]);
